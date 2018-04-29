ee31e6cb9fb248c96cef0c8a04f06d410452e4a3
JCFlores <jflores717@gmail.com>
2018-04-29 09:27:57 -0500

    fixed cannot GET issue with react router and not rendering after refresh

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -28,6 +29,7 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
+    historyApiFallback: true,
     contentBase: './dist',
     hot: true
   }
