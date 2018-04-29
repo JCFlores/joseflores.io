f25c15d27c91029c2afe2a98b99f936e4b1c5c8a
JCFlores <jflores717@gmail.com>
2018-04-28 20:53:29 -0500

    deleted title styled component, completed sidebar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,59 +8,58 @@ import regular from "@fortawesome/fontawesome-free-regular";
 import styled from "styled-components";
 
 const Nav = styled.nav`
-  background-color: #d75656;
-  height: auto;
-  min-height: 100vh;
-  display: inline-block;
-  padding: 1%;
-  width: 4rem;
+  height: 100%;
+  width: 13rem;
   position: fixed;
   top: 0;
   left: 0;
+  background-color: #CC6666;
 `;
 
-const Title = styled.h1`
-  font-size: 30px;
-  display: inline-block;
-  vertical-align: middle;
+const SplitNav = Nav.extend`
+  background-color: #fff;
 `;
 
 const NavUl = styled.ul`
+  position: relative;
   text-align: center;
+  height: 100%;
+  width: 100%;
 `;
 
 const NavItem = styled.a`
-  display: inline-block;
-  margin: 0px 16px;
+  display: block;
+  padding-top: 8%;
   cursor: pointer;
   color: #222;
+  height: 15%;
 `;
 
 class NavBar extends React.Component {
   render() {
     return (
-          <Nav>
-            <NavUl>
-              <NavItem href="https://www.twitter.com/joseflores_io">
-                <FontAwesomeIcon icon={["fab", "twitter"]} size="8x" />
-              </NavItem>
-              <NavItem href="https://www.github.com/JCFlores">
-                <FontAwesomeIcon icon={["fab", "github"]} size="8x" />
-              </NavItem>
-              <NavItem href="#">
-                <FontAwesomeIcon icon={["fab", "linkedin"]} size="8x" />
-              </NavItem>
-              <NavItem href="#">
-                <FontAwesomeIcon icon={["fas", "envelope"]} size="8x" />
-              </NavItem>
-              <NavItem href="#">
-                <FontAwesomeIcon icon={["far", "id-card"]} size="8x" />
-              </NavItem>
-              <NavItem href="#">
-                <FontAwesomeIcon icon={["far", "file-code"]} size="8x" />
-              </NavItem>
-            </NavUl>
-          </Nav>
+      <Nav>
+        <NavUl>
+          <NavItem href="/about">
+            <FontAwesomeIcon icon={["far", "id-card"]} size="7x" />
+          </NavItem>
+          <NavItem href="/portfolio">
+            <FontAwesomeIcon icon={["far", "file-code"]} size="7x" />
+          </NavItem>
+          <NavItem href="/contact">
+            <FontAwesomeIcon icon={["fas", "envelope"]} size="7x" />
+          </NavItem>
+          <NavItem href="https://www.github.com/JCFlores">
+            <FontAwesomeIcon icon={["fab", "github"]} size="7x" />
+          </NavItem>
+          <NavItem href="https://www.linkedin.com/in/jose-c-flores/">
+            <FontAwesomeIcon icon={["fab", "linkedin"]} size="7x" />
+          </NavItem>
+          <NavItem href="https://www.twitter.com/joseflores_io">
+            <FontAwesomeIcon icon={["fab", "twitter"]} size="7x" />
+          </NavItem>
+        </NavUl>
+      </Nav>
     );
   }
 }
